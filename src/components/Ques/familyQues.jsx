c3b82b1f4bb3887d8c433fbeb644eba2ece1e913
Anamika Aggarwal <150609132+Anamika1608@users.noreply.github.com>
2024-08-18 23:20:07 +0530

    Update familyQues.jsx

--- src/components/Ques/familyQues.jsx
+++ src/components/Ques/familyQues.jsx
@@ -106,7 +106,7 @@ function QuesFamily() {
                             transition={{ duration: 0.3 }}
                         >
                             <div className="text-center  ">
-                                <div className="mb-7 text-4xl font-semibold font-rocaLt">
+                            <div className="mb-10 text-2xl sm:text-3xl md:text-4xl font-semibold font-rocaLt">
                                     <span>{`${currentQuestion + 1}) `} &nbsp; </span> {questions[currentQuestion].question}
                                 </div>
                             </div>
@@ -114,7 +114,7 @@ function QuesFamily() {
                                 {questions[currentQuestion].options.map((option, index) => (
                                     <div className='mt-5 text-xl text-center mb-10' key={index}>
                                         <button onClick={() => handleAnswerClick(option)}>
-                                            <div className='bg-[#e6eaf0] bg-opacity-30 hover:bg-opacity-80 border-2 border-[#c3ccdb] w-80 m-3 p-3 rounded-lg'>
+                                        <div className='bg-[#e6eaf0] bg-opacity-30 hover:bg-opacity-80 border-2 border-[#c3ccdb] w-60 sm:w-72 m-3 p-3 rounded-lg'>
                                                 {option}
                                             </div>
                                         </button>
@@ -125,18 +125,18 @@ function QuesFamily() {
                     </AnimatePresence>
                 ) : (
                     <div className="text-center">
-                    <div className='text-4xl font-bold font-rocaLt mb-7 bg-[]'>Thank you for completing the questionnaire !</div>
+                   <div className='text-3xl sm:text-3xl md:text-4xl font-bold font-rocaLt mb-7 '>Thank you for completing the questionnaire !</div>
                     <div className='text-2xl font-semibold mb-7'>Your Analysis:</div>
                     {isLoading ? (
                         <div className="">Loading...</div>
                     ) : (
-                        <div className='text-xl mx-52 rounded-2xl bg-[#c7ebf2] bg-opacity-70 p-5 font-semibold'>
-                            <Markdown>{llmResponse}</Markdown>
-                        </div>
+                        <div className='text-base sm:text-lg md:text-xl sm:mx-8 md:mx-16 lg:mx-52 rounded-2xl bg-[#c7ebf2] bg-opacity-70 p-5 font-semibold'>
+                        <Markdown>{llmResponse}</Markdown>
+                    </div>
                     )}
                     <div className="">
                         <Link to="/book-session">
-                            <button className="mt-10 mr-20  md:mt-14 px-4 md:px-6 py-2 md:py-2 text-xl sm:text-2xl md:text-2xl lg:text-2xl bg-[#e6eddd] text-black rounded-2xl hover:bg-[#dfedce] hover:text-black transition duration-300">
+                            <button className="mt-10 md:mt-14 px-4 md:px-6 py-2 md:py-2 text-xl sm:text-2xl md:text-2xl lg:text-2xl bg-[#e6eddd] text-black rounded-2xl hover:bg-[#dfedce] hover:text-black transition duration-300">
                                 Book a Session
                             </button>
                         </Link>
